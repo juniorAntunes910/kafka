9e7b2e66d9ee35814e58cb4d15ac2164b54b46ec
Junior Antunes <jugabriel2612@gmail.com>
2026-08-11 09:45:03 -0300

    feat(project): Init project

---
+++ src/main/java/com/junior/airplane_panel/AirplanePanelApplication.java
@@ -0,0 +1,13 @@
+package com.junior.airplane_panel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplanePanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplanePanelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junior/airplane_panel/AirplanePanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.airplane_panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplanePanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
